b41d3f9e982adcc3a8ef7be16d77292591742218
LNTN <j251282@gmail.com>
2017-03-31 21:07:03 +0700

    readonly wake time input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -282,7 +282,7 @@
                         <label for="meal-time-input" class="sr-only">Time</label>
                         <div class="input-group clockpicker" data-placement="right" data-align="top"
                              data-autoclose="true">
-                            <input id="wake-time-input" type="text" class="form-control"
+                            <input id="wake-time-input" type="text" class="form-control" readonly="true"
                                    value="{{ Carbon::now()->format('H:i') }}">
                             <span class="input-group-addon"><i class="fa fa-clock-o" aria-hidden="true"></i></span>
                         </div>
