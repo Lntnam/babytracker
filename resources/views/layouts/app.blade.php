8ba627a79ff05b4bac1f69bc58e42c07b4b20f7a
LNTN <j251282@gmail.com>
2017-04-01 02:43:53 +0700

    menu aligned right on bigger screen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,16 +15,14 @@
 <body>
 
 <nav class="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
-    <div class="navbar-header">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="#">{{ $name }}</a>
-    </div>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">{{ $name }}</a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <a class="nav-link" href="{!! route('dashboard') !!}"><i class="fa fa-tachometer"
                                                                          aria-hidden="true"></i> Dashboard</a>
