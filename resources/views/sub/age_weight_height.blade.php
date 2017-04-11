38fb00480821cf2c780f21d1100a7e499192c63f
LNTN <j251282@gmail.com>
2017-04-11 08:50:16 +0700

    stop tracking height

--- resources/views/sub/age_weight_height.blade.php
+++ resources/views/sub/age_weight_height.blade.php
@@ -1,5 +1,5 @@
 <h5>
     <span class="badge badge-success">{{ $age->weeks . 'w ' . $age->daysExcludeWeeks . 'd' }}</span>
     <span class="badge badge-info"><span id="weight-value">{{ $weight }}</span>kg</span>
-    <span class="badge badge-info"><span id="height-value">{{ $height }}</span>cm</span>
+    {{--<span class="badge badge-info"><span id="height-value">{{ $height }}</span>cm</span>--}}
 </h5>
